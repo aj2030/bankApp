e9b93512e191bc97e00ace64d53a10b78207351a
Ajitabh Sharma <ajitabh.mca@gmail.com>
2019-07-18 09:47:57 +0530

    Changing array value and print message

--- src/lib2/Utility.js
+++ src/lib2/Utility.js
@@ -2,9 +2,9 @@
  * http://usejsdoc.org/
  */
 
-var a = ["Ajitabh", "Kumar", "Sharma"]
+var a = ["Ajitabh", "Kumar", "Sharma", "Hello", "World"]
 
 //Looping
 for(var i=0; i < a.length; i++){
-	console.log("Text is: "+ a[i]);
+	console.log("Text present in array is: "+ a[i]);
 }
