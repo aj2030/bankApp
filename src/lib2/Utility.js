b8cf395c5241fac4df38c58385d3e768f7dec488
Ajitabh Sharma <ajitabh.mca@gmail.com>
2019-07-18 10:14:14 +0530

    Streamlining array and printing statement

--- src/lib2/Utility.js
+++ src/lib2/Utility.js
@@ -2,9 +2,9 @@
  * http://usejsdoc.org/
  */
 
-var a = ["Ajitabh", "Kumar", "Sharma", "Hello", "World"]
+var a = ["Manjit", "Sharma", "Hello", "World"]
 
 //Looping
-for(var i=0; i < a.length; i++){
-	console.log("Text present in array is: "+ a[i]);
+for(var j=0; j < a.length; j++){
+	console.log("Text present in array is: "+ a[j]);
 }
