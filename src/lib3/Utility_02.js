797099131534bbe4317470551502cf047fa8555e
Ajitabh Sharma <ajitabh.mca@gmail.com>
2019-07-19 15:40:56 +0530

    Commit #6: Changing Array and changing variable name in for loop

--- src/lib3/Utility_02.js
+++ src/lib3/Utility_02.js
@@ -2,9 +2,9 @@
  * http://usejsdoc.org/
  */
 
-var array_01 = ["Ajitabh", "Kumar", "Sharma"]
+var array_01 = ["Kumar", "Hello", "World"]
 
 // Iterating over array
-for(var i=0; i < array_01.lenght; i++){
-	console.log("Printing array element: " + array_01[i]);
+for(var j=0; j < array_01.lenght; j++){
+	console.log("Printing array element: " + array_01[j]);
 }
