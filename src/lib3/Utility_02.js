2e27cc8ea2ff06f57b4623f7220b4834caaadc6a
Ajitabh Sharma <ajitabh.mca@gmail.com>
2019-07-19 16:32:35 +0530

    More changes

--- src/lib3/Utility_02.js
+++ src/lib3/Utility_02.js
@@ -9,3 +9,8 @@ for(var i=0; i < array_01.lenght; i++){
 	console.log("Printing elements of arrays: " + array_01[i]);
 
 }
+
+for(var j=0; j < array_01.lenght; j++){
+	console.log("Printing elements of arrays: " + array_01[j]);
+
+}
