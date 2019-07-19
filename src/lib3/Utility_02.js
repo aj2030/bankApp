eb9a59cb4c9b97b4571d2348fc8f3546fbf901b9
raynav09 <53033426+raynav09@users.noreply.github.com>
2019-07-19 15:54:33 +0530

    Update Utility_02.js
    adding new array

--- src/lib3/Utility_02.js
+++ src/lib3/Utility_02.js
@@ -2,9 +2,9 @@
  * http://usejsdoc.org/
  */
 
-var array_01 = ["Ajitabh", "Kumar", "Sharma"]
+var array_01 = ["Ajitabh", "Kumar", "R","Raynav","S"]
 
 // Iterating over array
 for(var i=0; i < array_01.lenght; i++){
-	console.log("Printing array element: " + array_01[i]);
+	console.log("Printing elements of arry in sequentional manner: " + array_01[i]);
 }
