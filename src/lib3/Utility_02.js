108dbf607d5e6dab24f31dc2ec636d073f89f451
raynav09 <53033426+raynav09@users.noreply.github.com>
2019-07-19 16:30:39 +0530

    Update Utility_02.js
    updated array

--- src/lib3/Utility_02.js
+++ src/lib3/Utility_02.js
@@ -2,10 +2,10 @@
  * http://usejsdoc.org/
  */
 
-var array_01 = ["Kumar", "Hello", "World"]
+var array_01 = ["Kumar", "Hello", "World","sequence"]
 
 
 for(var i=0; i < array_01.lenght; i++){
-	console.log("Printing elements of array: " + array_01[i]);
+	console.log("Printing elements of arrays: " + array_01[i]);
 
 }
