a868c343ddd8228ce7c70e9e8d785938492876eb
Ajitabh Sharma <ajitabh.mca@gmail.com>
2019-07-19 16:16:51 +0530

    Again changing printing message

--- src/lib3/Utility_02.js
+++ src/lib3/Utility_02.js
@@ -6,6 +6,6 @@ var array_01 = ["Kumar", "Hello", "World"]
 
 
 for(var i=0; i < array_01.lenght; i++){
-	console.log("Printing elements of arry in sequentional manner: " + array_01[i]);
+	console.log("Printing elements: " + array_01[i]);
 
 }
